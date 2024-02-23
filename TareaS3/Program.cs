7d4eab4d08da1d1e93ba491234535382c6f61ab0
Mijeli <mlucerob@miumg.edu.gt>
2024-02-23 01:14:49 -0600

    Programa 2: validar edad para ingresar a un club.

--- TareaS3/Program.cs
+++ TareaS3/Program.cs
@@ -1,28 +1,15 @@
-﻿int num1, num2, num3, mayor;
+﻿int edad;
 
 try
 {
-    Console.WriteLine("Ingrese el primer número: ");
-    num1=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Ingrese su edad: ");
+   edad = Convert.ToInt32(Console.ReadLine());
 
-    Console.WriteLine("Ingrese el segundo número: ");
-    num2 = Convert.ToInt32(Console.ReadLine());
-
-    Console.WriteLine("Ingrese el tercer número: ");
-    num3 = Convert.ToInt32(Console.ReadLine());
-
-    if (num1 == num2 && num2 == num3){
-        mayor = num1;
-        Console.WriteLine("Los tres números son iguales");
-    }else if (num1 > num2&& num1>num3){
-        mayor = num1;
-    }else if (num1 < num2 && num2 >= num3){
-        mayor = num2;
+    if (edad>=18){
+        Console.WriteLine("Edad adecuada. Ingreso al club permitido.");
     }else{
-        mayor = num3;
+        Console.WriteLine("No alcanza la edad mínima requerida. Ingreso al club NO permitido.");
     }
-
-    Console.WriteLine("El numero mayor es: "+mayor);
 }
 catch(Exception error)
 {
