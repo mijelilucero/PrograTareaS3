fd5f1a0f2ec5ae0994413a8f42b00c534498a6a4
Mijeli <mlucerob@miumg.edu.gt>
2024-02-23 02:14:01 -0600

    Programa 7: calcular el área de una figura geométrica.

--- TareaS3/Program.cs
+++ TareaS3/Program.cs
@@ -1,19 +1,35 @@
-﻿Decimal monto;
-int edad;
+﻿String figura;
+Double bas, altura, lado, radio, area=0;
+const double Pi = 3.14159;
 
 try
 {
-    Console.WriteLine("Ingrese el monto del prestamo: ");
-    monto=Convert.ToDecimal(Console.ReadLine());
+    Console.WriteLine("Ingrese el tipo de figura geomética (triangulo/cuadrado/circulo): ");
+    figura=Console.ReadLine().ToLower();
 
-    Console.WriteLine("Ingrese su edad: ");
-    edad = Convert.ToInt32(Console.ReadLine());
+    if (figura=="triangulo"){
+        Console.WriteLine("Ingrese la base: ");
+        bas = Convert.ToDouble(Console.ReadLine());
 
-    if (monto<5000||edad>60){
-        Console.WriteLine("Solicitud de prestamo aprobada.");
+        Console.WriteLine("Ingrese la altura: ");
+        altura = Convert.ToDouble(Console.ReadLine());
+
+        area = (bas * altura) / 2;
+    }else if (figura=="cuadrado"){
+        Console.WriteLine("Ingrese el lado: ");
+       lado = Convert.ToDouble(Console.ReadLine());
+
+        area = lado*lado;
+     }else if (figura == "circulo"){
+        Console.WriteLine("Ingrese el radio: ");
+        radio = Convert.ToDouble(Console.ReadLine());
+
+        area = Pi*(radio*radio);
     }else{
-        Console.WriteLine("Solicitud de prestamo rechazada.");
+        Console.WriteLine("Figura geométrica no valida.");
     }
+
+    Console.WriteLine("El area del {0} es de: {1}",figura, area);
 }
 catch(Exception error)
 {
